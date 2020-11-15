148a7ddd698ff4693f45b6192bf53fdaa7864e1b
Valentin Degenne <vdegenne@gmail.com>
2020-11-15 15:50:04 +0100

    selectCrossHair

--- content.js
+++ content.js
@@ -1,41 +1,36 @@
-function main() {
-  const priceTickerEl = document.querySelector('#price-ticker');
+const priceTickerEl = document.querySelector('#price-ticker');
 
-  function preventDefault(e) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    e.stopPropagation();
-    return;
-  }
-
-  window.addEventListener('keypress',function (e) {
-    // if (e.shiftKey) {
-    if (e.key === 'f') {
-      preventDefault(e);
-      // e.preventDefault();
-      // e.stopImmediatePropagation();
-      priceTickerEl.click();
-    }
-
-    if (e.key === 'F') {
-      document.querySelector('[data-testlabel="100%"]').click();
-    }
-
-    if (e.key === 'c') {
-      const order = document.querySelector('.order-close-button');
-      if (order) {
-        order.click();
-      }
-    }
-
-    if (e.key === 'b') {
-      document.querySelector('[data-testlabel="buy"]').click();
-    }
-    if (e.key === 's') {
-      document.querySelector('[data-testlabel="sell"]').click();
-    }
-    // }
-  })
+function selectCrossHair () {
+  document.querySelector('.icon-drawing-crosshair').dispatchEvent(new MouseEvent('mouseup', { bubbles: true }));
 }
 
-main()
+
+window.addEventListener('keypress', function (e) {
+  if (e.key === 'f') {
+    priceTickerEl.click();
+
+    // prevent tool change in the chart frame
+    selectCrossHair();
+  }
+
+  if (e.key === 'F') {
+    document.querySelector('[data-testlabel="100%"]').click();
+  }
+
+  if (e.key === 'c') {
+    const order = document.querySelector('.order-close-button');
+    if (order) {
+      order.click();
+      selectCrossHair()
+    }
+
+    // setTimeout(selectCrossHair, 500)
+  }
+
+  if (e.key === 'b') {
+    document.querySelector('[data-testlabel="buy"]').click();
+  }
+  if (e.key === 's') {
+    document.querySelector('[data-testlabel="sell"]').click();
+  }
+})
